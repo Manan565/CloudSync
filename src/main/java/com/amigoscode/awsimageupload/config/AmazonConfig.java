0d8823401d6487df21fea0481bcfefe2e1ba9dd7
Manan565 <mananmehta634@gmail.com>
2025-04-08 16:31:29 -0700

    API key fix

--- src/main/java/com/amigoscode/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/amigoscode/awsimageupload/config/AmazonConfig.java
@@ -14,8 +14,8 @@ public class AmazonConfig {
     @Bean
     public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-          "Replace with your own",
-          "Replace with your own"
+          "LATER",
+          "REPLACE WITH MINE"
         );
         return AmazonS3ClientBuilder
                 .standard()
